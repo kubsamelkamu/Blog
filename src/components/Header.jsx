3545acdda7089595b23c1e213d0913407c580e95
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-09-06 14:18:09 +0300

    added  menu bar functionality to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
+import { useState } from 'react';
 import { useTheme } from '../contexts/useTheme';
-import { FaSun, FaMoon } from 'react-icons/fa'; 
+import { FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
     const { theme, toggleTheme } = useTheme();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    if (!theme || !toggleTheme) {
-        return null; 
-    }
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <header className={`p-4 shadow-md ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'} transition-colors duration-300`}>
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">
-                    My Blog
-                </div>
-                <nav>
-                    <ul className="flex space-x-5">
+                    Blog
+                    <button 
+                        onClick={handleMenuToggle}
+                        className="block lg:hidden p-2 rounded-full focus:outline-none"
+                        aria-label="Toggle menu"
+                        >
+                        {isMenuOpen ? <FaTimes size={25} /> : <FaBars size={25} />}
+                    </button>
+
+                <nav className={`lg:flex lg:space-x-3 ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
+                    <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-3">
                         <li>
                             <a href="#" className="hover:text-gray-300">Home</a>
                         </li>
@@ -30,12 +39,15 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>
+                </div>
+                
+            
                 <button 
                     onClick={toggleTheme}
                     aria-label={theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
                     className={`p-2 rounded-full focus:outline-none transition-colors duration-300 ${theme === 'light' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}
                 >
-                    {theme === 'light' ? <FaMoon size={18} /> : <FaSun size={18} />}
+                    {theme === 'light' ? <FaMoon size={20} /> : <FaSun size={20} />}
                 </button>
             </div>
         </header>
