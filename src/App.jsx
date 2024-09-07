bc3646f5f1fb311a0a95ef4426396c8b646b82ec
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-09-07 10:50:03 +0300

    set the Router for page Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Blog from "./pages/Blog";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Home from "./pages/Home";
+import Profile from "./pages/user/Profile";
+import { ThemeProvider } from "./contexts/themecontext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <Router>
+      <ThemeProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header/>
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/blog" element={<Blog/>}/>
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/edit-post/:id" element={<EditPost />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </ThemeProvider>
+    </Router>
   )
+  
 }
 
-export default App
+export default App;
