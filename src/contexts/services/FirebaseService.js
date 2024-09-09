a9fecce34fd85debef01bf56989114762bb7f89f
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-09-09 11:19:10 +0300

    use getStorage

--- src/contexts/services/FirebaseService.js
+++ src/contexts/services/FirebaseService.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth} from 'firebase/auth';
-import { getStorage } from 'firebase/storage'; 
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and export it
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
